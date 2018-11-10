443971a242bf6b995531082034675f75042049e8
Student 200871 <200871@student.pwr.edu.pl>
2018-11-10 01:01:11 +0100

    Add

---
+++ kuznia/SentimentData.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.ML.Runtime.Api;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace kuznia
+{
+    public class SentimentData
+    {
+        [Column(ordinal: "0", name: "Label")]
+        public float Sentiment;
+        [Column(ordinal: "1")]
+        public string SentimentText;
+    }
+
+    public class SentimentPrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public bool Prediction { get; set; }
+
+        [ColumnName("Probability")]
+        public float Probability { get; set; }
+
+        [ColumnName("Score")]
+        public float Score { get; set; }
+    }
+
+
+}
